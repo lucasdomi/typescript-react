b217fc345ca4322e3adf31775b3ebfad2f564be0
Lucas Domingues <lucasdomi@globo.com>
2019-09-15 01:16:12 -0300

    Cria um componente stateless para renderizar itens

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -4,7 +4,7 @@ import { bindActionCreators, Dispatch } from 'redux';
 
 import { Repository } from '../../store/ducks/repositories/types';
 import { ApplicationState } from '../../store';
-
+import RepositoryItem from '../RepositoryItem';
 import * as RepositoriesActions from '../../store/ducks/repositories/actions';
 
 interface StateProps {
@@ -30,7 +30,7 @@ class RepositoryList extends Component<Props> {
     return (
       <ul>
         {repositories.map((repository) => (
-          <li>{repository.name}</li>
+          <RepositoryItem key={repository.id} repository={repository} />
         ))}
       </ul>
     );
