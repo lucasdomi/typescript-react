13d1cbe200b9d4f1aa33f7db902986f7151c85f7
Lucas Domingues <lucasdomi@globo.com>
2019-09-15 01:09:22 -0300

    Configura redux-saga

--- src/store/ducks/repositories/index.ts
+++ src/store/ducks/repositories/index.ts
@@ -2,9 +2,7 @@ import { Reducer } from 'redux';
 import { RepositoriesState, RepositoriesTypes } from './types';
 
 const INITIAL_STATE: RepositoriesState = {
-  data: [{
-    id: 1, name: 'Teste Lucas',
-  }],
+  data: [],
   error: false,
   loading: false,
 };
